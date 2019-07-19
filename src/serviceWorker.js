6b4a11df95ea957d726ac5758599edc7d29581d4
Jason Majors <jasonrmajors@gmail.com>
2019-07-18 21:35:39 -0700

    Trigger A2HS prompt if user hasnt dismised it yet on revisit

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -51,13 +51,23 @@ export function register(config) {
         registerValidSW(swUrl, config);
       }
     });
-    // let deferredPrompt
     window.addEventListener('beforeinstallprompt', (e) => {
+      let deferredPrompt
       console.log('beforeinstallprompt fired')
       // Prevent Chrome 67 and earlier from automatically showing the prompt
       e.preventDefault();
       // Stash the event so it can be triggered later.
-      // deferredPrompt = e;
+      deferredPrompt = e;
+      deferredPrompt.prompt()
+      deferredPrompt.userChoice
+      .then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+      });
     });
   }
 }
